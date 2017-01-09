e283a15e01d2f6c7d8493d18f32d0befc629050c
Wissse <f_givehchian@yahoo.com>
2017-01-09 16:04:55 +0330

    Add files via upload

---
+++ models/LoginModel.php
@@ -0,0 +1,30 @@
+﻿<?php 
+class LoginModel extends CI_Model
+{
+	function __construct()
+	{
+		parent:: __construct();
+	}
+
+
+	function registration($user_data)
+	{
+		$all = array(
+		'name'=>$user_data['reg_name'],
+		'fname'=>$user_data['reg_fname'],
+		'phoneNumber'=>$user_data['reg_tel'],
+		'username'=>$user_data['reg_user'],
+		'password'=>$user_data['reg_pass'],	
+		'address'=>$user_data['reg_address'],
+		'email' => $user_data['reg_mail']
+		);
+		//print_r($all);
+		$res=$this->db->insert('user',$all);
+		return $res;
+	}
+
+	
+
+
+}
+?>
---
+++ models/ProfileModel.php
@@ -0,0 +1,31 @@
+﻿<?php 
+class ProfileModel extends CI_Model
+{
+	function __construct()
+	{
+		parent:: __construct();
+	}
+
+
+	function loadInfo($user)
+	{
+
+		 $sql = "SELECT * FROM `user` 
+			 WHERE `username` = '". $user ."'";
+    	$result = $this->db->query($sql);
+    	echo json_encode( $result->result());
+    
+	}
+
+	function updateInfo($username,$name,$fname,$address,$email,$tel)
+	{
+		$sql='UPDATE user SET  name = "'.$name.'" ,fname = "'.$fname .'",email = "'.$email .'",address = "'.$address .'" ,phoneNumber = "'.$tel.'" WHERE username = "'.$username .'"'; 
+	    $result = $this->db->query($sql);
+	    
+		
+	}
+	
+
+
+}
+?>
