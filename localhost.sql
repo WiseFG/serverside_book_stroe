6f4c944b36d879611c5422f94cb2f82f9d93ad11
bahareh helli <bahareh.helli@gmail.com>
2017-01-14 20:35:53 +0330

    Add files via upload

--- localhost.sql
+++ localhost.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 24, 2016 at 11:15 AM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Host: 127.0.0.1
+-- Generation Time: Jan 14, 2017 at 06:17 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `onlinebookstore`
 --
+CREATE DATABASE IF NOT EXISTS `onlinebookstore` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `onlinebookstore`;
 
 -- --------------------------------------------------------
 
@@ -26,7 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `book`
 --
 
-CREATE TABLE IF NOT EXISTS `book` (
+CREATE TABLE `book` (
   `id` bigint(20) NOT NULL,
   `name` varchar(64) NOT NULL,
   `publisher` varchar(64) NOT NULL,
@@ -35,8 +37,9 @@ CREATE TABLE IF NOT EXISTS `book` (
   `description` varchar(1024) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   `count` int(11) NOT NULL,
-  `addedDate` varchar(32) NOT NULL,
-  `sale` decimal(10,0) DEFAULT NULL
+  `sale` decimal(10,0) DEFAULT NULL,
+  `photo` varchar(256) DEFAULT NULL,
+  `cate` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -45,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `book` (
 -- Table structure for table `card`
 --
 
-CREATE TABLE IF NOT EXISTS `card` (
+CREATE TABLE `card` (
   `id` bigint(20) NOT NULL,
   `number` varchar(32) NOT NULL,
   `ownername` varchar(64) NOT NULL,
@@ -58,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `card` (
 -- Table structure for table `cart`
 --
 
-CREATE TABLE IF NOT EXISTS `cart` (
+CREATE TABLE `cart` (
   `id` bigint(20) NOT NULL,
   `userId` bigint(20) DEFAULT NULL,
   `date` date NOT NULL
@@ -70,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `cart` (
 -- Table structure for table `cartrow`
 --
 
-CREATE TABLE IF NOT EXISTS `cartrow` (
+CREATE TABLE `cartrow` (
   `id` bigint(20) NOT NULL,
   `count` int(11) NOT NULL,
   `bookId` bigint(20) NOT NULL,
@@ -80,28 +83,19 @@ CREATE TABLE IF NOT EXISTS `cartrow` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
---
-
-CREATE TABLE IF NOT EXISTS `category` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `bookid` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `invoice`
 --
 
-CREATE TABLE IF NOT EXISTS `invoice` (
+CREATE TABLE `invoice` (
   `id` bigint(20) NOT NULL,
   `userId` bigint(20) NOT NULL,
   `date` varchar(32) NOT NULL,
   `deliveryTime` varchar(32) DEFAULT NULL,
   `paymentmethodtype` tinyint(1) NOT NULL,
-  `cardid` bigint(20) DEFAULT NULL
+  `cardid` bigint(20) DEFAULT NULL,
+  `status` varchar(10) NOT NULL,
+  `place` varchar(15) NOT NULL,
+  `Delivered` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -110,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `invoice` (
 -- Table structure for table `invoicerow`
 --
 
-CREATE TABLE IF NOT EXISTS `invoicerow` (
+CREATE TABLE `invoicerow` (
   `id` bigint(20) NOT NULL,
   `count` int(11) NOT NULL,
   `bookId` bigint(20) NOT NULL,
@@ -123,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `invoicerow` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL,
   `username` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
@@ -133,15 +127,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `birthday` varchar(32) DEFAULT NULL,
   `gender` varchar(16) DEFAULT NULL,
   `role` varchar(32) NOT NULL,
-  `name` varchar(128) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `name` varchar(128) NOT NULL,
+  `fname` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `phoneNumber`, `address`, `email`, `birthday`, `gender`, `role`, `name`) VALUES
-(1, 'hhh', '333', NULL, NULL, 'hhh@www.nooo', NULL, NULL, '', '');
+INSERT INTO `user` (`id`, `username`, `password`, `phoneNumber`, `address`, `email`, `birthday`, `gender`, `role`, `name`, `fname`) VALUES
+(1, 'hhh', '333', NULL, NULL, 'hhh@www.nooo', NULL, NULL, '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -173,13 +168,6 @@ ALTER TABLE `cartrow`
   ADD PRIMARY KEY (`id`),
   ADD KEY `bookId` (`bookId`),
   ADD KEY `cartId` (`cartId`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `bookid` (`bookid`);
 
 --
 -- Indexes for table `invoice`
@@ -228,11 +216,6 @@ ALTER TABLE `cart`
 ALTER TABLE `cartrow`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
@@ -246,7 +229,7 @@ ALTER TABLE `invoicerow`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
@@ -263,12 +246,6 @@ ALTER TABLE `cart`
 ALTER TABLE `cartrow`
   ADD CONSTRAINT `cartrow_ibfk_1` FOREIGN KEY (`cartId`) REFERENCES `cart` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `cartrow_ibfk_2` FOREIGN KEY (`bookId`) REFERENCES `book` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `category`
---
-ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`bookid`) REFERENCES `book` (`id`);
 
 --
 -- Constraints for table `invoice`
